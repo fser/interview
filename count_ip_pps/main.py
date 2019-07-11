55b03d3f2686f8b2ef35bc825f3331f3a3e8635b
Hauctopuce <lucas.mattei@epita.fr>
2019-07-11 17:56:35 +0200

    Scanning ips.dat

--- count_ip_pps/main.py
+++ count_ip_pps/main.py
@@ -1,5 +1,33 @@
+import re
+
+def get_file_content(filename):
+    f = open(filename, 'r')
+    lines = f.readlines()
+    f.close
+
+    #Discarding the header
+    lines = lines[1:]
+    return lines
+
+
+def get_ips_list():
+    ip_dict = dict()
+    lines = get_file_content("ips.dat")
+    if lines == None :
+        return None
+
+    for line in lines:
+        scan = re.match(r"(\S+) (\S+)$", line)
+        if scan :
+            ip_dict[scan.group(2)] = scan.group(1)
+
+    return ip_dict
+        
+
+
 def main():
-    print("Hello World !")
+    print(get_ips_list())
+    
 
 
 
