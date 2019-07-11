cc655f45b8e938d51ed3a31e0bdb361caa1a0a79
Hauctopuce <lucas.mattei@epita.fr>
2019-07-11 18:10:09 +0200

    scanning pps.dat

--- count_ip_pps/main.py
+++ count_ip_pps/main.py
@@ -1,5 +1,7 @@
 import re
 
+REGEX_TUPLE = r'(\S+) (\S+)$'
+
 def get_file_content(filename):
     f = open(filename, 'r')
     lines = f.readlines()
@@ -10,23 +12,40 @@ def get_file_content(filename):
     return lines
 
 
-def get_ips_list():
+def get_ips_dict():
     ip_dict = dict()
     lines = get_file_content("ips.dat")
     if lines == None :
         return None
 
     for line in lines:
-        scan = re.match(r"(\S+) (\S+)$", line)
+        scan = re.match(REGEX_TUPLE, line)
         if scan :
             ip_dict[scan.group(2)] = scan.group(1)
 
     return ip_dict
-        
+
+
+def get_pps_dict():
+    pps_dict = dict()
+    lines = get_file_content("pps.dat")
+    if lines == None:
+        return None
+    
+    for line in lines:
+        scan = re.match(REGEX_TUPLE, line)
+        if scan:
+            if pps_dict.get(scan.group(1)) == None:
+                pps_dict[scan.group(1)] = 0
+            pps_dict[scan.group(1)] += int(scan.group(2))
+
+    return pps_dict 
 
 
 def main():
-    print(get_ips_list())
+    ip_dict = get_ips_dict()
+    pps_dict = get_pps_dict()
+    print(pps_dict)
     
 
 
